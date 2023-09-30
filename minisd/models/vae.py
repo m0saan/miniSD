dfa556d0c517660b06af9f2244488efbd1aa6ada
__mo_san__ <moboustt@student.1337.ma>
2023-09-30 11:18:34 +0100

    first commit :xd

---
+++ minisd/models/vae.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class VaeEncoder(nn.Module):
+    def __init__(self, ) -> None:
+        super().__init__()
+
+class VaeDecoder(nn.Module):
+    def __init__(self, ) -> None:
+        super().__init__()
+
+
+class VAE(nn.Module):
+    
+    def __init__(self, encoder: VaeEncoder, decoder: VaeDecoder) -> None:
+        super().__init__()
+    

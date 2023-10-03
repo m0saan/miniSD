bc4a51d26291c5855517de1538010330b035c055
__mo_san__ <moboustt@student.1337.ma>
2023-10-04 00:04:44 +0100

    minimal changes

--- minisd/models/diffusion.py
+++ minisd/models/diffusion.py
@@ -3,7 +3,7 @@ import torch.nn.functional as F
 from torch import nn
 from unet import UNet, TimeEmbeddings, Head
 
-class DiffusionModel(nn.Module):
+class UnetDiffusionModel(nn.Module):
     """
     A PyTorch implementation of the Diffusion Model.
     """
@@ -18,7 +18,7 @@ class DiffusionModel(nn.Module):
     def forward(self, latents: torch.Tensor, time: torch.Tensor, context: torch.Tensor) -> torch.Tensor:
         # latents: (batch_size, latent_dim, height, width) Bx4x64x64
         # time: (1, 320)
-        # context: (batch_size, seq_len, hidden_size)
+        #  : (batch_size, seq_len, hidden_size)
         
         time_embed = self.time_embeddings(time) # (1, 320) -> (1, 1280)
         unet_out = self.unet(latents, time_embed, context) # (B, 4, 64, 64), (B, 320, 64, 64)
@@ -29,7 +29,7 @@ if __name__ == '__main__':
     x = torch.randn(1, 4, 64, 64).to('mps')
     t = torch.randn(1, 320).to('mps')
     context = torch.randn(1, 77, 768).to('mps')
-    model = DiffusionModel().to('mps')
+    model = UnetDiffusionModel().to('mps')
 
     print(x.shape)
     pred = model(x, t, context)

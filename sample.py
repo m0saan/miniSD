177e5e36f0474b8e34cfce1957d23a95cbe09640
__mo_san__ <moboustt@student.1337.ma>
2023-10-04 14:55:07 +0100

    toward first gen images :p

--- sample.py
+++ sample.py
@@ -1,28 +1,42 @@
 import argparse
 import pathlib
 import matplotlib.pyplot as plt
+import torch
 from datetime import datetime
+from PIL import Image
+from transformers import CLIPTokenizer
+
+from minisd.models import pipeline, clip, vae, diffusion, samplers
 
 def generate_image(prompt):
-    pass
+    device = 'mps' if torch.has_mps else 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f'>>> Using device: {device}')
+    
+    tokenizer = CLIPTokenizer(vocab_file='data/tokenizer/vocab.json', merges_file='data/tokenizer/merges.txt')
+    generator = torch.Generator(device=device)
+    sampler = samplers.DDPMSampler(generator=generator)
+    pipe = pipeline.MiniSDPipeline(tokenizer=tokenizer, sampler=sampler)
+    image = pipe(prompt).images[0]
+    plt.imshow(image)
 
 def main():
-    parser = argparse.ArgumentParser(description='Generate images from a text prompt.')
-    parser.add_argument('--prompt', type=str, required=True, help='The text prompt for image generation.')
+    # parser = argparse.ArgumentParser(description='Generate images from a text prompt.')
+    # parser.add_argument('--prompt', type=str, required=True, help='The text prompt for image generation.')
     
-    args = parser.parse_args()
-    prompt = args.prompt
+    # args = parser.parse_args()
+    # prompt = args.prompt
 
-    print(f'Generating image for prompt: {prompt}')
-    generate_image(prompt)
+    # print(f'Generating image for prompt: {prompt}')
+    # generate_image(prompt)
     
-    timestamp = datetime.now().strftime('%Y%m%d')
-    sanitized_prompt = prompt.replace(' ', '_').replace('/', '').replace('\\', '')[:50]
-    filename = f'{sanitized_prompt}_{timestamp}.png'
-    path = pathlib.Path('data/output_data/')
+    # timestamp = datetime.now().strftime('%Y%m%d')
+    # sanitized_prompt = prompt.replace(' ', '_').replace('/', '').replace('\\', '')[:50]
+    # filename = f'{sanitized_prompt}_{timestamp}.png'
+    # path = pathlib.Path('data/output_data/')
 
-    plt.savefig(path/filename)
-    print(f'Image saved as {filename}')
+    # plt.savefig(path/filename)
+    # print(f'Image saved as {filename}')
+    generate_image('a photo of a happy dog')
 
 if __name__ == '__main__':
     main()
